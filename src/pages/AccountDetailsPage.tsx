4578229bcd121e9663446c7eef88489f82f21b66
Roman Cherednik <roman-tik@users.noreply.github.com>
2021-04-21 18:18:42 +0300

    patch symbols (#1)
    * Update TopAccountsCard.tsx
    
    * Update UpgradeableLoaderAccountSection.tsx
    
    * Update UnknownAccountCard.tsx
    
    * Update StakeAccountSection.tsx
    
    * Update StakeHistoryCard.tsx
    
    * Update SupplyCard.tsx
    
    * Update Account.tsx
    
    * Update TransferDetailsCard.tsx
    
    * Update WithdrawDetailsCard.tsx
    
    * Update CreateDetailsCard.tsx
    
    * Update NonceWithdrawDetailsCard.tsx
    
    * Update SplitDetailsCard.tsx
    
    * Update TransferWithSeedDetailsCard.tsx
    
    * Update CreateWithSeedDetailsCard.tsx
    
    * Update TransactionDetailsPage.tsx
    
    * Update TokenAccountSection.tsx
    
    * Update AccountDetailsPage.tsx

--- src/pages/AccountDetailsPage.tsx
+++ src/pages/AccountDetailsPage.tsx
@@ -163,7 +163,7 @@ function DetailsSections({ pubkey, tab }: { pubkey: PublicKey; tab?: string }) {
       {flaggedAccounts.has(address) && (
         <div className="alert alert-danger alert-scam" role="alert">
           Warning! This account has been flagged by the community as a scam
-          account. Please be cautious sending SOL to this account.
+          account. Please be cautious sending assets to this account.
         </div>
       )}
       {<InfoSection account={account} />}
